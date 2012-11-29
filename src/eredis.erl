bc8bffc1bc8d7bf29ca2136ac97f1d5e8c9657ad
Jeremy Ong <jeremy@playmesh.com>
2012-11-28 16:52:38 -0800

    Add try catch block to eredis:to_binary/1

--- src/eredis.erl
+++ src/eredis.erl
@@ -116,7 +116,11 @@ to_bulk(B) when is_binary(B) ->
 %% term_to_binary/1. For floats, throws {cannot_store_floats, Float}
 %% as we do not want floats to be stored in Redis. Your future self
 %% will thank you for this.
-to_binary(X) when is_list(X)    -> list_to_binary(X);
+to_binary(X) when is_list(X)    -> try list_to_binary(X) of
+                                       Y -> Y
+                                   catch
+                                       error:badarg -> term_to_binary(X)
+                                   end;
 to_binary(X) when is_atom(X)    -> list_to_binary(atom_to_list(X));
 to_binary(X) when is_binary(X)  -> X;
 to_binary(X) when is_integer(X) -> list_to_binary(integer_to_list(X));
