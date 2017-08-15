666d979f513e3b27a7949e49c109f8d790fcadfe
Knut Nesheim <knutin@gmail.com>
2017-08-15 19:13:53 +0100

    Added send_timeout of 5s. Closes #95

--- include/eredis.hrl
+++ include/eredis.hrl
@@ -33,6 +33,8 @@
 
 -define(NL, "\r\n").
 
--define(SOCKET_OPTS, [binary, {active, once}, {packet, raw}, {reuseaddr, false}]).
+-define(SOCKET_OPTS, [binary, {active, once}, {packet, raw}, {reuseaddr, false},
+                      {send_timeout, ?SEND_TIMEOUT}]).
 
 -define(RECV_TIMEOUT, 5000).
+-define(SEND_TIMEOUT, 5000).
