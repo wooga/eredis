faf7154164ea36b2730572189790f347388a2ebe
Knut Nesheim <knutin@gmail.com>
2018-09-12 13:29:04 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eredis.Mixfile do
   def project do
     [
       app: :eredis,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.5.1",
       start_permanent: Mix.env == :prod,
       deps: deps()
